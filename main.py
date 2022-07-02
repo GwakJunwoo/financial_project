22f05ae15d302b6a8ae5cb5078fc854f73e33b47
lunar <lunarsabbth@gmail.com>
2022-07-02 21:31:18 +0900

    변경

---
+++ main.py
@@ -0,0 +1,125 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader as pdr
+import FinanceDataReader as fdr
+import yfinance as yf
+import investpy
+import pykrx
+import dart_fss as dart
+from datetime import datetime
+from openpyxl import Workbook, load_workbook
+from typing import Optional, Dict, List, Tuple, Union
+import pymysql.cursors
+
+
+class Mydb:
+    def __init__(self, host, user, db, password):
+        self.conn = pymysql.connect(host=host, user=user, db=db, password=password)
+        self.cursor = self.conn.cursor()
+
+    def insert(self, table, value_list: Union[List, Tuple], column_list: Optional[Union[List, Tuple]] = None):
+        try:
+            if column_list is None:
+                sql = "INSERT INTO {table} VALUES (" + ', '.join(value_list) + ")"
+            else:
+                sql = "INSERT INTO {table} (".format(table=table) + ', '.join(column_list) + ") VALUES (" \
+                      + ', '.join(value_list) + ")"
+            self.cursor.execute(sql)
+            self.conn.commit()
+
+        except Exception as e:
+            print(e)
+
+    def delete(self, table, condition):
+        try:
+            sql = "DELETE FROM {table} WHERE ".format(table=table) + condition
+            self.cursor.execute(sql)
+            self.conn.commit()
+
+        except Exception as e:
+            print(e)
+
+    def query(self, query):
+        try:
+            self.cursor.execute(query)
+            self.conn.commit()
+
+        except Exception as e:
+            print(e)
+
+    def view(self, table, condition):
+        try:
+            sql = "SELECT * FROM {table} ".format(table=table) + "WHERE " + condition
+            df = pd.read_sql(sql, self.conn)
+            print(df)
+
+        except Exception as e:
+            print(e)
+
+    def get_data(self, table, condition):
+        try:
+            sql = "SELECT * FROM {table} ".format(table=table) + "WHERE " + condition
+            df = pd.read_sql(sql, self.conn)
+            return df
+
+        except Exception as e:
+            print(e)
+
+    def table_status(self):
+        try:
+            sql = "SHOW TABLE STATUS"
+            return pd.read_sql(sql, self.conn)
+
+        except Exception as e:
+            print(e)
+
+    def columns_describe(self, table):
+        try:
+            sql = "DESCRIBE " + table
+            return pd.read_sql(sql, self.conn)
+
+        except Exception as e:
+            print(e)
+
+
+class Crawler:
+    def __init__(self, database: Mydb, data_vendor=None):
+        self.tickers = None
+        self.ticker_index = None
+
+        self.VENDOR = data_vendor
+        self.VENDOR_URL = ""
+        self.VENDOR_ID: int
+        self.db = database
+
+        if self.VENDOR == "Yahoo":
+            self.VENDOR_URL = "https://finance.yahoo.com/"
+            self.VENDOR_ID = 1
+
+        elif self.VENDOR == "Investing":
+            self.VENDOR_URL = "https://kr.investing.com/"
+            self.VENDOR_ID = 3
+
+        elif self.VENDOR == "Dart":
+            self.VENDOR_URL = "https://dart.fss.or.kr/"
+            self.VENDOR_ID = 5
+
+        elif self.VENDOR == "FinanceDataReader":
+            self.VENDOR_URL = "https://github.com/FinanceData/FinanceDataReader"
+            self.VENDOR_ID = 2
+
+        elif self.VENDOR == "pykrx":
+            self.VENDOR_URL = "https://github.com/sharebook-kr/pykrx"
+            self.VENDOR_ID = 4
+
+        else:
+            print('Unconfirmed source! Please check your data vendor!')
+
+    def set_tickers(self, ticker_list: Optional[List] = None):
+        if ticker_list is None:
+            all_tickers = pd.read_sql("SELECT ticker, id FROM security", self.db.conn)
+            self.ticker_index = dict(all_tickers.to_dict('split')['data'])
+            self.tickers = list(self.ticker_index.keys())
+
+        else:
